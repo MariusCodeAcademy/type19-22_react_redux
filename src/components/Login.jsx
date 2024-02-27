8bd551a40ef7be208fd5d23fc4b73b2d414b74f9
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-27 11:31:44 +0200

    login works

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,42 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Button from './UI/Button';
+import { authAction } from '../store';
 
 const Login = () => {
+  const loginStatus = useSelector((state) => state.auth.isLoggedIn);
+
+  const dispatch = useDispatch();
+
   const hangleLogin = (ev) => {
     ev.preventDefault();
     console.log('pateikta');
+    dispatch(authAction.login());
   };
   return (
     <div>
-      <h2>Login</h2>
-      <form noValidate onSubmit={hangleLogin} className='flex flex-col gap-3 md:w-1/2'>
-        <input
-          type='email'
-          placeholder='Email'
-          className='border border-gray-300 rounded-md p-2 mb-2'
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          className='border border-gray-300 rounded-md p-2 mb-2'
-        />
-        <Button type='submit'>Submit</Button>
-      </form>
+      <h2 className='text-2xl my-3'>Login</h2>
+      <p className='tex-2xl'>Login status is: {loginStatus.toString()}</p>
+      {loginStatus && (
+        <h2 className='text-3xl border rounded-md text-center text-green-700 bg-green-200 py-5'>
+          You are logged in
+        </h2>
+      )}
+
+      {loginStatus === false && (
+        <form noValidate onSubmit={hangleLogin} className='flex flex-col gap-3 md:w-1/2'>
+          <input
+            type='email'
+            placeholder='Email'
+            className='border border-gray-300 rounded-md p-2 mb-2'
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            className='border border-gray-300 rounded-md p-2 mb-2'
+          />
+          <Button type='submit'>Submit</Button>
+        </form>
+      )}
     </div>
   );
 };
