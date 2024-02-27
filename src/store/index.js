0a94e7b6d49abe88608c449ff2d70278ba0402a5
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-27 10:34:25 +0200

    slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,53 @@
-import { legacy_createStore as createStore } from 'redux';
+// import { createStore } from 'redux';
 
-const initState = { counterValue: 10, showCounter: true };
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const initCounterState = { counterValue: 5, showCounter: true };
+
+// createSlice.reducer naudoja vidini paketa immer kuris neleidzia mutuoti state
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initCounterState,
+  reducers: {
+    up(state) {
+      // veiksmas up del immer nera mutacija
+      state.counterValue++;
+    },
+    down(state) {
+      state.showCounter = state.counterValue - 1;
+    },
+    upBy() {},
+    toggle() {},
+  },
+});
 
 // reducer fn
-const counterReducer = (state = initState, action) => {
-  // niedada tiesiogiai nemodifikuojam state
-  // state.counterValue++ // no no
-  // state.showCounter = true // no no
-  console.log('state ===', state);
-  console.log('action ===', action);
+// const counterReducer = (state = initState, action) => {
+//   // niedada tiesiogiai nemodifikuojam state
+//   // state.counterValue++ // no no
+//   // state.showCounter = true // no no
+//   console.log('state ===', state);
+//   console.log('action ===', action);
 
-  switch (action.type) {
-    case 'UP':
-      return { ...state, counterValue: state.counterValue + 1 };
-    case 'DOWN':
-      return { ...state, counterValue: state.counterValue - 1 };
-    case 'UP_BY':
-      return { ...state, counterValue: state.counterValue + action.payload };
-    case 'TOGGLE':
-      return { ...state, showCounter: !state.showCounter };
-    default:
-      console.warn(`toks veiksmas nerastas "${action.type}"`);
-      return state;
-  }
-};
+//   switch (action.type) {
+//     case 'UP':
+//       return { ...state, counterValue: state.counterValue + 1 };
+//     case 'DOWN':
+//       return { ...state, counterValue: state.counterValue - 1 };
+//     case 'UP_BY':
+//       return { ...state, counterValue: state.counterValue + action.payload };
+//     case 'TOGGLE':
+//       return { ...state, showCounter: !state.showCounter };
+//     default:
+//       console.warn(`toks veiksmas nerastas "${action.type}"`);
+//       return state;
+//   }
+// };
 
 // sukuriam pagr reducer kintamaji
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
