88a87e3b1de85688a6c8b7e0b502a31621f33ac3
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-27 11:34:49 +0200

    header login state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import { useSelector } from 'react-redux';
 
 export default function Header() {
   const counter = useSelector((state) => state.counter.counterValue);
+  const isAuth = useSelector((state) => state.auth.isLoggedIn);
+  console.log('isAuth ===', isAuth);
   return (
     <header className='bg-slate-300'>
       <div className='container flex justify-between items-center'>
@@ -15,9 +17,14 @@ export default function Header() {
           <a className='text-lg p-3 hover:bg-slate-700 hover:text-white' to={'/'}>
             Home
           </a>
-          <a className='text-lg p-3 hover:bg-slate-700 hover:text-white' to={'/products'}>
-            Products
-          </a>
+          {isAuth === false && (
+            <a className='text-lg p-3 hover:bg-slate-700 hover:text-white' to={'/login'}>
+              Login
+            </a>
+          )}
+          {isAuth && (
+            <span className='text-lg p-3 hover:bg-slate-700 hover:text-white'>Logout</span>
+          )}
         </nav>
       </div>
     </header>
